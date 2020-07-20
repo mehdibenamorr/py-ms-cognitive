d49846f49922c04ae260a2cfc78adf02cfe06849
Mehdi Ben Amor <mehdi.benamor@uni-passau.de>
2020-07-20 13:45:49 +0200

    uri fix

--- py_ms_cognitive/py_ms_cognitive_search/py_ms_cognitive_video_search.py
+++ py_ms_cognitive/py_ms_cognitive_search/py_ms_cognitive_video_search.py
@@ -2,6 +2,7 @@ import requests, requests.utils
 from .py_ms_cognitive_search import PyMsCognitiveSearch
 from .py_ms_cognitive_search import QueryChecker
 
+
 ##
 ##
 ## Video Search
@@ -11,9 +12,9 @@ from .py_ms_cognitive_search import QueryChecker
 class PyMsCognitiveVideoException(Exception):
     pass
 
-class PyMsCognitiveVideoSearch(PyMsCognitiveSearch):
 
-    SEARCH_VIDEO_BASE = 'https://api.cognitive.microsoft.com/bing/v5.0/videos/search'
+class PyMsCognitiveVideoSearch(PyMsCognitiveSearch):
+    SEARCH_VIDEO_BASE = 'https://api.cognitive.microsoft.com/bing/v7.0/videos/search'
 
     def __init__(self, api_key, query, custom_params={}, silent_fail=False):
         query_url = self.SEARCH_VIDEO_BASE
@@ -25,18 +26,19 @@ class PyMsCognitiveVideoSearch(PyMsCognitiveSearch):
         '''
         limit = min(limit, self.MAX_SEARCH_PER_QUERY)
         payload = {
-          'q' : self.query,
-          'count' : limit, #currently 50 is max per search.
-          'offset': self.current_offset,
+            'q': self.query,
+            'count': limit,  # currently 50 is max per search.
+            'offset': self.current_offset,
         }
         payload.update(self.CUSTOM_PARAMS)
 
-        headers = { 'Ocp-Apim-Subscription-Key' : self.api_key }
+        headers = {'Ocp-Apim-Subscription-Key': self.api_key}
         response = requests.get(self.QUERY_URL, params=payload, headers=headers)
         json_results = self.get_json_results(response)
         packaged_results = [VideoResult(single_result_json) for single_result_json in json_results["value"]]
         self.current_offset += min(50, limit, len(packaged_results))
         return packaged_results
+
 
 class VideoResult(object):
     '''
@@ -64,4 +66,4 @@ class VideoResult(object):
         self.host_page_url = result.get('hostPageUrl')
         self.web_search_url = result.get('webSearchUrl')
         self.video_id = result.get('videoId')
-        self.description= result.get('description')
+        self.description = result.get('description')
